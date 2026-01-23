1f77529caa2a9ef39ca159797a192e0d460e1386
MinGH <mgh110@naver.com>
2026-01-24 03:26:00 +0900

    [feat] : Map Data Controller 추가

---
+++ Tool/Public/Panel_MapDataController.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "ImGui_Panel.h"
+
+NS_BEGIN(Tool)
+
+class CPanel_MapDataController : public CImGui_Panel
+{
+	using Super = CImGui_Panel;
+protected:
+	explicit CPanel_MapDataController(const _char* pLabel, CLevel* pOwner, ID3D11Device* pDevice, ID3D11DeviceContext* pDeviceContext);
+	virtual ~CPanel_MapDataController() = default;
+public:
+	virtual HRESULT Render(CToolObject* pGo)override;
+	virtual void Update(const _float fTimeDelta)override;
+private:
+
+public:
+	static  CPanel_MapDataController* Create(const _char* pLabel, CLevel* pOwner, ID3D11Device* pDevice, ID3D11DeviceContext* pDeviceContext);
+	virtual void Free() override;
+};
+
+NS_END
+
