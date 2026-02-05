7067e9eacaffb21c0094c85a2daa64e3c5e16455
Eunbi <lucyban@naver.com>
2026-02-05 15:40:33 +0900

    [ refactor ] : CState_ComboBase 완성

--- Client/Private/State_ComboBase.cpp
+++ Client/Private/State_ComboBase.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "State_ComboBase.h"
 
+#include "PlayerControlContext.h"
+
 CState_ComboBase::CState_ComboBase(CActionState* pOwnerComponent, const string& strName)
 	: Super(pOwnerComponent, strName)
 {
@@ -15,6 +17,7 @@ HRESULT CState_ComboBase::Initialize(void* pArg)
 	m_ComboTimes[0] = pDesc->vCombo_CheckTimes.x;
 	m_ComboTimes[1] = pDesc->vCombo_CheckTimes.y;
 	m_ComboTimes[2] = pDesc->vCombo_CheckTimes.z;
+	m_ComboTimes[3] = pDesc->vCombo_CheckTimes.w;
 
 	return S_OK;
 }
@@ -32,16 +35,19 @@ HRESULT CState_ComboBase::Start(void* pArg, _bool bForce)
 	if (FAILED(Super::Start(pArg, bForce)))
 		return E_FAIL;
 
-	m_bComboTime = false;
+	m_bComboTime	= true;
+	m_iComboCount	= 1;
 
 	return S_OK;
 }
 
 void CState_ComboBase::Update(const _float fTimeDelta)
 {
+	Super::Update(fTimeDelta);
+
 	Count_ComboTime(fTimeDelta);
 
-	Super::Update(fTimeDelta);
+	Check_Combo();
 }
 
 HRESULT CState_ComboBase::End()
@@ -54,44 +60,69 @@ HRESULT CState_ComboBase::End()
 
 void CState_ComboBase::Change_NextCombo()
 {
-	// 다음 콤보 change
-	m_iComboCount++;
-	m_fTimeAcc = 0.f;
+	if (m_iComboCount == 4)
+	{
+		Change_FirstCombo();
+		return;
+	}
+
+	// 시간들 초기화
+	m_tKeyTimer.fTimeAcc	= 0.f;
+	m_fStateElapsed			= 0.f;
+
+	// combo 관련 변수 설정
+	m_iMainAnimIdx = m_iComboCount++;
+	m_bNextCombo = false;
+
 	Request_ChangeAnimation(m_vecMainAnims[m_iComboCount], true, false, true);
+}
+
+void CState_ComboBase::Change_FirstCombo()
+{
+	// 시간들 초기화
+	m_tKeyTimer.fTimeAcc = 0.f;
+	m_fStateElapsed = 0.f;
+
+	// combo 관련 변수 설정
+	m_iComboCount = 1;
+	m_iMainAnimIdx = 0;
+	m_bNextCombo = false;
+
+	Request_ChangeAnimation(m_vecMainAnims[0], true, false, true);
 }
 
 void CState_ComboBase::Count_ComboTime(const _float fTimeDelta)
 {
-	// 4번째 일때는 하지 않음
-	if (m_iComboCount == 4)
-		return;
-
-	// key cool time이 다 되었는데 아직 combotime이라면
-	if (m_tKeyTimer.fTimeAcc / m_tKeyTimer.fMaxTime == 1.f &&
-		m_bComboTime)
+	// 만약 아직 combo check time이고, key가 눌렸다면
+	if (Key_Input(ENUM_TO_UINT(CControlContext::CONTROL_KEY::LATT))
+		 && m_fStateElapsed <= m_ComboTimes[m_iComboCount - 1])
 	{
-		// comboTime을 카운트 한다
-		m_fTimeAcc += fTimeDelta;
-		if (m_fTimeAcc >= m_ComboTimes[m_iComboCount -1])
-		{
-			m_bComboTime = false;
-		}
+		m_bNextCombo = true;
 	}
 }
 
-void CState_ComboBase::Change_PlayerState(STATEKEY eKey)
+void CState_ComboBase::Check_Combo()
 {
-	// 만약 combo Time이 아닌데 공격 키를 눌러도 변화하지 않음
-	if (STATEKEY::LM == eKey)
+	// 너무 시간이 지나면 combo end
+	if (m_fStateElapsed > m_ComboTimes[m_iComboCount - 1] +1.5f)
 	{
-		if (m_bComboTime)
-		{
-			Change_NextCombo();
-			return;
-		}
+		return;
 	}
 
-	Super::Change_PlayerState(eKey);
+	// combo change 시간 ~ end 시간 사이
+	else if (m_fStateElapsed > m_ComboTimes[m_iComboCount - 1] + 0.5f &&
+		Key_Input(ENUM_TO_UINT(CControlContext::CONTROL_KEY::LATT)))
+	{
+		Change_FirstCombo();
+		return;
+	}
+
+	// combo change 시간이 다 되었는데
+	else if (m_fStateElapsed > m_ComboTimes[m_iComboCount - 1] &&
+		m_bNextCombo)
+	{
+		Change_NextCombo();
+	}
 }
 
 void CState_ComboBase::Free()
--- Client/Public/State_ComboBase.h
+++ Client/Public/State_ComboBase.h
@@ -12,7 +12,7 @@ class CState_ComboBase abstract : public CStateBase_Player
 public:
 	typedef struct tagComboStateDesc : public CStateBase_Player::PLAYER_STATEBASE_DESC
 	{
-		Vec3 vCombo_CheckTimes = Vec3::Zero;
+		Vec4 vCombo_CheckTimes = Vec4::Zero;
 	}PLAYER_COMBOBASE_DESC;
 
 protected:
@@ -28,18 +28,19 @@ public:
 	virtual HRESULT End() override;
 
 protected:
-	void Change_NextCombo();
-
-private:
-	std::array<_float, 3>		m_ComboTimes; // 4번째 콤보에서는 다음 콤보를 받지 않음
-	_float		m_fTimeAcc		= { 0.f };
+	std::array<_float, 4>		m_ComboTimes; // 4번째 콤보에서는 다음 콤보를 받지 않음
 	_bool		m_bComboTime	= { false };
+	_bool		m_bNextCombo	= { false };
 
-	_uint		m_iComboCount		= { 1 };
+	_uint		m_iComboCount	= { 1 };
+
+protected:
+	void Change_NextCombo();
+	void Change_FirstCombo();
+	void Check_Combo();
 
 private:
 	void			Count_ComboTime(const _float fTimeDelta);
-	virtual void	Change_PlayerState(STATEKEY eKey) override;	// change 랩핑 함수 : 필요시 오버라이드
 
 public:
 	virtual void Free() override;
