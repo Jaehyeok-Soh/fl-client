8d2df59b48eb675e861ecc38c5ae0bed23b7ab4a
KBJ_Laptop <dzzzing@naver.com>
2026-02-10 17:15:01 +0900

    [ docs ] : _DEBUG 매크로 추고

--- Engine/Private/PhysicsAttackOverlap.cpp
+++ Engine/Private/PhysicsAttackOverlap.cpp
@@ -31,11 +31,13 @@ HRESULT CPhysicsAttackOverlap::Initialize(void* pArg)
 	return S_OK;
 }
 
+#ifdef _DEBUG
 void CPhysicsAttackOverlap::Render()
 {
 	for (auto& event : m_activeEvents)
 		event->Render();
 }
+#endif
 
 void CPhysicsAttackOverlap::Awake()
 {
