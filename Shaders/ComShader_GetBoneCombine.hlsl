4c75a1bfd56963821895828fa67aceb7c0efd106
DESKTOP-S82RJ0D\admin <lucybankim@gmail.com>
2026-02-11 18:17:22 +0900

    [ fix ] :GetBone CS 수정

--- Shaders/ComShader_GetBoneCombine.hlsl
+++ Shaders/ComShader_GetBoneCombine.hlsl
@@ -20,10 +20,10 @@ StructuredBuffer<BONE_MAT>      SELECTED_COMBINETRANSFORMS_SRV;
 void CS_Main(uint3 id : SV_DispatchThreadID)
 {
     // bone index 추출
-    uint index = IMMU_BONEINDIECS[id.x];
+    uint index = IMMU_BONEINDIECS[id.x].iNeedBoneIdx;
     
     // n번째에 해당 bone index의 matrix 저장
-    SELECTED_COMBINETRANSFORMS[id.x].matBoneTransform = MU_COMBINEDBONES[index];
+    SELECTED_COMBINETRANSFORMS[id.x].matBoneTransform = MU_COMBINEDBONES[index].matBoneTransform;
 }
 
 technique11 T0
