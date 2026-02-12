9d12ce3814aba3e0991d3c1881794eb1ed0e8f52
DESKTOP-S82RJ0D\admin <lucybankim@gmail.com>
2026-02-12 23:19:34 +0900

    [ feat ] : CSword 클래스 생성

---
+++ Client/Private/Sword.cpp
@@ -0,0 +1,116 @@
+#include "pch.h"
+#include "Sword.h"
+
+CSword::CSword(ID3D11Device* pDevice, ID3D11DeviceContext* pDeviceContext)
+	:Super(pDevice, pDeviceContext, Weapon_Type::SWORD)
+{
+}
+
+CSword::CSword(const CSword& rhs)
+	:Super(rhs)
+{
+}
+
+HRESULT CSword::Initialize_Prototype()
+{
+	if (FAILED(Super::Initialize_Prototype()))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+HRESULT CSword::Initialize(void* pArg)
+{
+	if (!pArg)
+		return E_FAIL;
+
+	if (FAILED(Super::Initialize(pArg)))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+HRESULT CSword::Awake(const _uint iCurrentLevelIndex)
+{
+	if (FAILED(Super::Awake(iCurrentLevelIndex)))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+void CSword::Update_Priority(_float fTimeDelta)
+{
+	Super::Update_Priority(fTimeDelta);
+}
+
+void CSword::Update(_float fTimeDelta)
+{
+	Super::Update(fTimeDelta);
+}
+
+void CSword::Update_Late(_float fTimeDelta)
+{
+	Super::Update_Late(fTimeDelta);
+}
+
+void CSword::Ready_Before_Render(_float fTimeDelta)
+{
+	Super::Ready_Before_Render(fTimeDelta);
+}
+
+void CSword::OnCollision(_uint iMyColliderLayer, CGameObject* pOther)
+{
+	Super::OnCollision(iMyColliderLayer, pOther);
+}
+
+void CSword::OnCollision_Enter(_uint iMyColliderLayer, CGameObject* pOther)
+{
+	Super::OnCollision_Enter(iMyColliderLayer, pOther);
+}
+
+void CSword::OnCollision_Exit(_uint iMyColliderLayer, CGameObject* pOther)
+{
+	Super::OnCollision_Exit(iMyColliderLayer, pOther);
+}
+
+void CSword::OnTrigger_Enter(_uint iMyColliderLayer, CGameObject* pOther)
+{
+	Super::OnTrigger_Enter(iMyColliderLayer, pOther);
+}
+
+void CSword::OnTrigger_Exit(_uint iMyColliderLayer, CGameObject* pOther)
+{
+	Super::OnTrigger_Exit(iMyColliderLayer, pOther);
+}
+
+HRESULT CSword::Render()
+{
+	return Super::Render();
+}
+
+CSword* CSword::Create(ID3D11Device* pDevice, ID3D11DeviceContext* pDeviceContext)
+{
+	CSword* pInsatnce = new CSword(pDevice, pDeviceContext);
+	if (FAILED(pInsatnce->Initialize_Prototype()))
+	{
+		MSG_BOX("CSword::Create, Failed");
+		Safe_Release(pInsatnce);
+	}
+	return pInsatnce;
+}
+
+CGameObject* CSword::Clone(void* pArg)
+{
+	CSword* pClone = new CSword(*this);
+	if (FAILED(pClone->Initialize(pArg)))
+	{
+		MSG_BOX("CSword::Clone, Failed");
+		Safe_Release(pClone);
+	}
+	return pClone;
+}
+
+void CSword::Free()
+{
+	__super::Free();
+}
