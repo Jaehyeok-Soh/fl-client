c6daefa1dc2f0b1a09904b260d8a607d53805fe5
DESKTOP-S82RJ0D\admin <lucybankim@gmail.com>
2026-02-15 15:04:34 +0900

    [ style ] : CStatComponent tap 적용

--- Client/Public/StatComponent.h
+++ Client/Public/StatComponent.h
@@ -11,23 +11,28 @@ public:
 	{
 		_int iHealth = { 0 };
 	}STATCOMP_DESC;
-private:
+protected:
 	CStatComponent();
 	explicit CStatComponent(const CStatComponent& rhs);
 	virtual ~CStatComponent() = default;
 
 	virtual HRESULT Initialize_Prototype() override;
+
 public:
 	virtual HRESULT Awake(_uint iLevelIndex) override;
 	virtual HRESULT Initialize(void* pArg) override;
 	virtual void Update(const _float fTimeDelta) override;
+
+public:
 	_float Get_HealthRatio() const { return (_float)m_iHealth / (_float)m_iMaxHealth; }
 	void Add_Health(_int iHealth) { m_iHealth += iHealth; }
 	_bool Is_HealthZero() const { return m_iHealth <= 0; }
-private:
-	CGameInstance* m_pGameInstance = { nullptr };
-	_int m_iMaxHealth = { 0 };
-	_int m_iHealth = { 0 };
+
+protected:
+	CGameInstance*	m_pGameInstance		= { nullptr };
+	_int			m_iMaxHealth		= { 0 };
+	_int			m_iHealth			= { 0 };
+
 public:
 	static CStatComponent* Create();
 	virtual CComponent* Clone(void* pArg) override;
