050b95f32f56c64a3853c57d3e4b7ed5d4aac601
DESKTOP-S82RJ0D\admin <lucybankim@gmail.com>
2026-02-21 16:07:46 +0900

    [ feat ] : CMyStat Engine으로 내림

---
+++ Engine/Private/MyStat.cpp
@@ -0,0 +1,103 @@
+#include "Engine_pch.h"
+#include "MyStat.h"
+#include "GameInstance.h"
+
+CMyStat::CMyStat()
+	: Super()
+{
+
+}
+
+CMyStat::CMyStat(const CMyStat& rhs)
+	: Super(rhs)
+	, m_fMaxHealth(rhs.m_fMaxHealth)
+{
+
+}
+
+HRESULT CMyStat::Initialize_Prototype()
+{
+	if (FAILED(Super::Initialize_Prototype()))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+HRESULT CMyStat::Initialize(void* pArg)
+{
+	if (pArg == nullptr)
+		return E_FAIL;
+
+	if (FAILED(Super::Initialize(pArg)))
+		return E_FAIL;
+
+	STATCOMP_DESC* pDesc	= static_cast<STATCOMP_DESC*>(pArg);
+	m_fMaxHealth			= pDesc->fMaxHp;
+	m_fHealth				= pDesc->fMaxHp;
+
+	m_fAttack = pDesc->fAttack;
+	m_fSheild = pDesc->fSheild;
+	if (m_fSheild > 0)
+		m_bSheildOn = true;
+
+	return S_OK;
+}
+
+void CMyStat::Add_Health(_float fHealth)
+{
+	if (fHealth > 0)
+		Add_Hp(fHealth);
+
+	else
+		Sub_Hp(fHealth);
+}
+
+void CMyStat::Add_Hp(_float fHealth)
+{
+	m_fHealth += fHealth;
+
+	// max hp 넘었는지 검사
+	if (m_fHealth > m_fMaxHealth)
+		m_fHealth = m_fMaxHealth;
+}
+
+void CMyStat::Sub_Hp(_float fHealth)
+{
+	m_fHealth += fHealth;
+
+	// sheild 발동중이라면 : sheild 값 더해줌
+	if (m_bSheildOn)
+		m_fHealth += m_fSheild;
+
+	// 만약 음수가 되었다면 0으로 맞추기
+	if (m_fHealth < 0)
+		m_fHealth = 0;
+}
+
+CMyStat* CMyStat::Create()
+{
+	CMyStat* pInstance = new CMyStat();
+	if (FAILED(pInstance->Initialize_Prototype()))
+	{
+		MSG_BOX("CMyStat::Create, Failed");
+		Safe_Release(pInstance);
+	}
+	return pInstance;
+}
+
+CComponent* CMyStat::Clone(void* pArg)
+{
+	CMyStat* pInstance = new CMyStat();
+	if (FAILED(pInstance->Initialize(pArg)))
+	{
+		MSG_BOX("CMyStat::Clone, Failed");
+		Safe_Release(pInstance);
+	}
+	return pInstance;
+}
+
+void CMyStat::Free()
+{
+	Safe_Release(m_pGameInstance);
+	Super::Free();
+}
---
+++ Engine/Public/MyStat.h
@@ -0,0 +1,57 @@
+#pragma once
+#include "Component.h"
+
+NS_BEGIN(Engine)
+
+class ENGINE_DLL CMyStat : public CComponent
+{
+	using Super = CComponent;
+public:
+	typedef struct tagStatComponentDesc
+	{
+		_float	fMaxHp		= { 0 };
+		_float	fAttack		= { 0 };
+		_float	fSheild		= { 0 };
+	}STATCOMP_DESC;
+protected:
+	CMyStat();
+	explicit CMyStat(const CMyStat& rhs);
+	virtual ~CMyStat() = default;
+
+	virtual HRESULT Initialize_Prototype() override;
+	virtual HRESULT Initialize(void* pArg) override;
+
+public:
+	virtual void Add_Health(_float fHealth); // 만약 객체 마다 hp 증감을 다르게 다루고 싶을 수 있으니 가상함수로
+	
+	// getter setter funcs
+public:
+	_float Get_HealthRatio() const { return m_fHealth / m_fMaxHealth; }
+	_bool Is_HealthZero() const { return m_fHealth <= 0; }
+
+	_float Get_Attack() const { return m_fAttack; }
+	_float Get_Sheild() const { return m_fSheild; }
+
+	void Set_Attack(_float fAttack) { m_fAttack = fAttack; }
+	void Set_SheildFlag(_bool bSheildOn) { m_bSheildOn = bSheildOn; }
+
+protected:
+	_float			m_fMaxHealth		= { -1.f }; // Max가 0이면 안되어서 음수로 해둠
+	_float			m_fHealth			= { 0 };
+
+	_float			m_fAttack			= { 0 };	// 공격력 : 플레이어인 경우, 현재 state에 따른 공격량을 바꿔줄 예정.
+
+	_float			m_fSheild			= { 0 };	// 방어력
+	_bool			m_bSheildOn			= { false };
+
+protected:
+	virtual void Add_Hp(_float fHealth);		// 매게변수 값이 양수일때
+	virtual void Sub_Hp(_float fHealth);		// 매게변수 값이 음수일때
+
+public:
+	static CMyStat* Create();
+	virtual CComponent* Clone(void* pArg) override;
+	virtual void Free() override;
+};
+
+NS_END
