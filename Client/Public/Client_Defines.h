93fdbe37af79555676edf0231455c262958d79ed
MinGH <mgh110@naver.com>
2026-02-25 10:54:19 +0900

    [ feat ] : Test Level 추가

--- Client/Public/Client_Defines.h
+++ Client/Public/Client_Defines.h
@@ -21,6 +21,7 @@ namespace Client
 		TUTORIAL_VILLAGE,	/* 튜토리얼 처음 진입되는 Level */
 		TUTORIAL_BOSS,		/* 튜토리얼 진입 이후 Boss전 가는 Level Type */
 		SQUARE,				/* 광장 */
+		Test,
 		END
 	};
 	inline constexpr size_t g_iLevelType_Count = static_cast<size_t>(ELevelType::END);
