d83da101990dfcc95182b5ba78df01dee7ae00a8
DESKTOP-S82RJ0D\admin <lucybankim@gmail.com>
2026-03-14 16:43:27 +0900

    [ fix ] : channel 복사 생성 막음

--- Engine/Public/Model.h
+++ Engine/Public/Model.h
@@ -342,6 +342,9 @@ private:
 
 private:
 	_int								m_iRootBoneIdx				= { -1 };
+	Vec3								m_vPreMainPosition = { Vec3::Zero };
+	Vec3								m_vPreBlendPosition = { Vec3::Zero };
+	Vec3								m_vPreMixPosition = { Vec3::Zero };
 
 	// mix anim
 	vector<_int>						m_vecMixAnimIndices;
