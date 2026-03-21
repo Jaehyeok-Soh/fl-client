03484f3fc169f5e9c36c7fd877db035ddcb8e337
DESKTOP-S82RJ0D\admin <lucybankim@gmail.com>
2026-03-21 20:31:20 +0900

    [ feat ] : Dual Hit Font Color 변경

--- Client/Private/Monster_Base.cpp
+++ Client/Private/Monster_Base.cpp
@@ -664,7 +664,7 @@ void CMonster_Base::OnHit_Dual(const HIT_DESC& hitDesc)
 		UI_PREFAB_DATA tPrefabData = {};
 		UI_DAMAGEFONT_PREFAB_DATA Desc = {};
 		Desc.iDamage = static_cast<_uint>(hitDesc.fFinalDamage);	// 데미지 폰트에 뜰 숫자 // 플레이어 공격력 // 랜덤은 보여주기용
-		Desc.vFontColor = Vec4{ 0.f, 0.82f, 1.f, 1.f };			// 데미지 폰트 색 // 캐릭터 고유 색
+		Desc.vFontColor = Vec4{ 0.f, 0.85f, 0.6f, 1.f };			// 데미지 폰트 색 // 캐릭터 고유 색
 		Desc.vHitPos = hitDesc.vHitPoint;							// 데미지 폰트를 띄울 World 위치 // 
 		Desc.vRandOffset = Vec3{
 			m_pGameInstance->Rand_Float(-1.f, 1.f),
